e3461eec9df0dd2ceb1d0958066479c60a632d13
Ashlie Warren <ashliewantstocode@gmail.com>
2019-06-13 14:51:42 -0700

    push

---
+++ models/index.js
@@ -0,0 +1,7 @@
+// Exports the module.export object containing all models created. 
+// *Note* there is no index inside module because that is where you are housing it.)
+
+module.exports = {
+    Article: require("./Article"),
+    Note: require("./Note")
+};
